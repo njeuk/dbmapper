fd9dbb48cacdbd860a2de529b9f148c2dd2e111b
Nick Edwards <nick@comdevelopment.com>
2014-09-11 22:43:44 +0100

    travis to use postgres 9.3

--- script/prepare_build.sh
+++ script/prepare_build.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env sh
 
-echo "preparing postgresql configs"
-
 psql -c 'create database dbmappersamples;' -U postgres
-psql -c "alter database dbmappersamples set timezone to 'GMT'" -U
 
-cat "/etc/postgresql/9.1/main/pg_hba.conf"
 
 
 
