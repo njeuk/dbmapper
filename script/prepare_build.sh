ea00234098ee03ab64622ee50541f13882dffd2f
Nick Edwards <nick@comdevelopment.com>
2014-09-11 21:17:40 +0100

    dumping travis db connections

--- script/prepare_build.sh
+++ script/prepare_build.sh
@@ -3,6 +3,9 @@
 echo "preparing postgresql configs"
 
 psql -c 'create database dbmappersamples;' -U postgres
-psql -c "alter database dbmappersamples set timezone to 'GMT'" -U postgres
+psql -c "alter database dbmappersamples set timezone to 'GMT'" -U
+
+cat "/etc/postgresql/9.1/main/pg_hba.conf"
+
 
 
